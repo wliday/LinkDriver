1e21639c3307e619cbed57222c0593a58371dc9a
wei <liwei.day@gmail.com>
2016-05-18 19:19:21 -0400

    update

--- step_entry_point.rb
+++ step_entry_point.rb
@@ -33,49 +33,49 @@ linkedin_page.search_result_page.wait_until_present
 filters = config_reader["search"]["filters"]
 logger.info("started setting up filters")
 filters.each do |key, value|
-	case key
+  case key
 
-		when 'type'
-			linkedin_page.search_type_selector(value).click unless value.nil?
-			sleep 5
+    when 'type'
+      linkedin_page.search_type_selector(value).click unless value.nil?
+      sleep 5
 
-			Watir::Wait.while {linkedin_page.search_loading}
+      Watir::Wait.while { linkedin_page.search_loading }
       sleep 1
 
-			logger.info("set type as #{value}")
+      logger.info("set type as #{value}")
 
-		when 'relationship_to_uncheck'
-			value.split(DELIMITER).each do |val|
+    when 'relationship_to_uncheck'
+      value.split(DELIMITER).each do |val|
 
         linkedin_page.search_relationship_selector(val).clear
-				sleep 4
+        sleep 4
         logger.info("unchecked relationship #{val}")
 
-				Watir::Wait.while {linkedin_page.search_loading}
-				sleep 1
+        Watir::Wait.while { linkedin_page.search_loading }
+        sleep 1
 
-			end
-		when 'relationship_to_check'
-			value.split(DELIMITER).each do |val|
-				linkedin_page.search_relationship_selector(val).set
-				sleep 8
+      end
+    when 'relationship_to_check'
+      value.split(DELIMITER).each do |val|
+        linkedin_page.search_relationship_selector(val).set
+        sleep 8
         logger.info("checked relationship #{val}")
 
-				Watir::Wait.while {linkedin_page.search_loading}
-				sleep 4
-			end
-		when 'location'
-			value.split(DELIMITER).each do |val|
-				linkedin_page.search_locations(val).set
-				sleep 8
+        Watir::Wait.while { linkedin_page.search_loading }
+        sleep 4
+      end
+    when 'location'
+      value.split(DELIMITER).each do |val|
+        linkedin_page.search_locations(val).set
+        sleep 8
         logger.info("set location as #{val}")
 
-				Watir::Wait.while {linkedin_page.search_loading}
-				sleep 1
-			end
-		else
-			logger.warn("Some of your filters are ignored")
-	end
+        Watir::Wait.while { linkedin_page.search_loading }
+        sleep 1
+      end
+    else
+      logger.warn("Some of your filters are ignored")
+  end
 end
 
 # start sending invitation
@@ -83,83 +83,83 @@ index = 0
 page = 1
 logger.info("started sending invitation")
 while true
-	begin
-		results = linkedin_page.search_results(index)
-		if !results.nil?
-			results.each_with_index do |li, i|
-				begin
-					index = index + 1
-					if !((li.class_name.include? "people") && (linkedin_page.name_exist? li) && (linkedin_page.description_exist? li))
-						next;
-					end
-					name = linkedin_page.name_of_person(li)
-					description = linkedin_page.description_of_person(li)
-					if records.exists?(name, description) then
-						logger.info("We already sent invitiation to #{name} before.")
-						next
-					else
-						records.add(name, description)
-					end
+  begin
+    results = linkedin_page.search_results(index)
+    if !results.nil?
+      results.each_with_index do |li, i|
+        begin
+          index = index + 1
+          if !((li.class_name.include? "people") && (linkedin_page.name_exist? li) && (linkedin_page.description_exist? li))
+            next;
+          end
+          name = linkedin_page.name_of_person(li)
+          description = linkedin_page.description_of_person(li)
+          if records.exists?(name, description) then
+            logger.info("We already sent invitiation to #{name} before.")
+            next
+          else
+            records.add(name, description)
+          end
 
-					if !linkedin_page.blue_button_exists?(li) then
-						logger.debug("can not find blue button for #{name}")
-						next
-					else
-						blue_button = linkedin_page.blue_button(li)
-					end
+          if !linkedin_page.blue_button_exists?(li) then
+            logger.debug("can not find blue button for #{name}")
+            next
+          else
+            blue_button = linkedin_page.blue_button(li)
+          end
 
-					if blue_button.text == 'Message'
-						logger.info("Already Connected with #{name}")
-					elsif blue_button.text == 'Follow'
-						blue_button.click
-						logger.info("Now following #{name}")
-					elsif blue_button.text == 'Connect'
-						blue_button.when_present.click
-						sleep 2
-						if linkedin_page.need_email_page?
-							browser.back
-							logger.info("Need email address to connect with #{name}")
-							linkedin_page.search_result_page.wait_until_present
-							break
-						else
-							logger.info("Invitation has been sent to #{name}")
-						end
-					end
-				rescue
-					logger.error("Had exception during #{li.to_s}")
-					next;
-				end
-			end
+          if blue_button.text == 'Message'
+            logger.info("Already Connected with #{name}")
+          elsif blue_button.text == 'Follow'
+            blue_button.click
+            logger.info("Now following #{name}")
+          elsif blue_button.text == 'Connect'
+            blue_button.when_present.click
+            sleep 2
+            if linkedin_page.need_email_page?
+              browser.back
+              logger.info("Need email address to connect with #{name}")
+              linkedin_page.search_result_page.wait_until_present
+              break
+            else
+              logger.info("Invitation has been sent to #{name}")
+            end
+          end
+        rescue
+          logger.error("Had exception during #{li.to_s}")
+          next;
+        end
+      end
 
-			# navigate to next page
-			if index > (results.length - 1)
-				puts "Page #{page}, index: #{index}, length: #{results.length}"
-				if linkedin_page.next_page.exists? then
-					linkedin_page.next_page.click
-					page = page + 1
-					index = 0
-					logger.info("next page loading...")
+      # navigate to next page
+      if index > (results.length - 1)
+        puts "Page #{page}, index: #{index}, length: #{results.length}"
+        if linkedin_page.next_page.exists? then
+          linkedin_page.next_page.click
+          page = page + 1
+          index = 0
+          logger.info("next page loading...")
 
-					Watir::Wait.while {linkedin_page.search_loading}
-					sleep 5
-				else
-					break
-				end
-			end
-		end
-	rescue
-		logger.error("Exception during fetching results in this page")
-		if linkedin_page.next_page.exists? then
-			linkedin_page.next_page.click
-			page = page + 1
-			index = 0
-			logger.info("next page loading...")
-			sleep 2
-			Watir::Wait.while {linkedin_page.search_loading}
-		else
-			break
-		end
-	end
+          Watir::Wait.while { linkedin_page.search_loading }
+          sleep 5
+        else
+          break
+        end
+      end
+    end
+  rescue
+    logger.error("Exception during fetching results in this page")
+    if linkedin_page.next_page.exists? then
+      linkedin_page.next_page.click
+      page = page + 1
+      index = 0
+      logger.info("next page loading...")
+      sleep 2
+      Watir::Wait.while { linkedin_page.search_loading }
+    else
+      break
+    end
+  end
 end
 browser.close
 records.file_close
